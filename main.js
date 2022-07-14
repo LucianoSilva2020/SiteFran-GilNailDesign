034a8b88768d50ac3b09a6ad22f2f6f3e83f427a
Luciano Silva <93166470+LucianoSilva2020@users.noreply.github.com>
2022-07-14 17:33:58 -0300

    .

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 function myFunction() {
-    var x = document.getElementById("Topnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("Topnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
+
+const item = document.querySelectorAll("[data-anime");
+
+const animeScrol = () => {
+  const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+  item.forEach(element => {
+    if (windowTop > element.offsetTop) {
+      element.classList.add('animate');
+    } else {
+      element.classList.remove("animate");
+    }
+  });
+};
+
+animeScrol();
+
+window.addEventListener("scroll", () => {
+  animeScrol();
+})
